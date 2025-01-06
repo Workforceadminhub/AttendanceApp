3816ed78f2ed5f47c932c2c9321391805dfc50e1
Chidi Enwereji <chidibede@gmail.com>
2025-01-06 11:06:33 +0100

    add login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,31 @@
 const Login = () => {
   return (
-    <div className="p-4 bg-white rounded shadow-md max-w-sm mx-auto">
-      <h2 className="text-xl font-semibold mb-4">Login to Mark Attendance</h2>
-      <input
-        type="text"
-        placeholder="Enter your ID"
-        className="w-full p-2 border rounded mb-4"
-      />
-      <button className="w-full bg-blue-600 text-white py-2 rounded">
-        Login
-      </button>
+    <div className="bg-gray-50 min-h-screen flex items-center justify-center">
+      <div className="bg-white rounded-lg shadow-md p-8 w-[700px] h-[250px]">
+        <h1 className="text-2xl font-bold text-center mb-6">
+          Login to Mark Attendance
+        </h1>
+        <div>
+          <div className="mb-4">
+            <label for="id" className="block text-sm font-medium text-gray-700">
+              ID
+            </label>
+            <input
+              type="text"
+              id="id"
+              name="id"
+              placeholder="Enter your ID"
+              className="mt-1 block h-12 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-black text-white py-2 px-4 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600"
+          >
+            Login
+          </button>
+        </div>
+      </div>
     </div>
   );
 };

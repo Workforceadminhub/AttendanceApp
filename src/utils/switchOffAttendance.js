fba85f5f36879a6d7764e993bca4522325f68fc9
Chidi Enwereji <chidibede@gmail.com>
2025-02-02 20:01:12 +0100

    ability to switch off

--- src/utils/switchOffAttendance.js
+++ src/utils/switchOffAttendance.js
@@ -2,7 +2,7 @@ import { supabase } from "../services/supabaseClient";
 
 export const switchOffAttendance = async () => {
   // Logic to switch off attendance
-  const { data } = await supabase.from("expirytables").select().eq("id", 1);
+  const { data } = await supabase.from("expirytable").select().eq("id", 1);
   const attendanceIsClosed = data && data.length > 0 ? data[0].isClosed : false;
   return attendanceIsClosed;
 };

ba7d5d07e2b99cb0f8bb6c5c2077202e82a9c424
Chidi Enwereji <chidibede@gmail.com>
2025-02-09 17:17:51 +0100

    check for admin

--- src/utils/checkAdminStatus.js
+++ src/utils/checkAdminStatus.js
@@ -4,6 +4,6 @@ import { getDepartmentByUser } from "./getDepartment";
 export const checkAdminStatus = (pathname) => {
   const team = getDepartmentByUser(pathname);
   const isChurchAdmin = team.department === ADMIN_ENUMS.ADMIN_DEPARTMENT;
-  const isAdmin = pathname.includes("admin/") ? true : false;
+  const isAdmin = pathname.includes("/admin/") ? true : false;
   return isChurchAdmin || isAdmin;
 };

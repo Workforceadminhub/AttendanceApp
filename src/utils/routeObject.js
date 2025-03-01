6d94af5e405f33c9bad4679ae332594c88e1f465
Chidi Enwereji <chidibede@gmail.com>
2025-03-01 17:07:27 +0100

    create new departmemnts under senior eadership

--- src/utils/routeObject.js
+++ src/utils/routeObject.js
@@ -107,6 +107,8 @@ export const routeObject = [
   { department: "Venue Management - Emmanuel team", route: "/vmgtemma", team: "Programs" },
   { department: "Venue Management - Boluwatife team", route: "/vmgtbolu", team: "Programs" },
   { department: "Venue Management - Feyisayo Phillip team", route: "/vmgtfeyi", team: "Programs" },
+  { department: "Pastoral leader", route: "/pastoralleader", team: "Senior Leadership" },
+  { department: "Directional leader", route: "/directionalleader", team: "Senior Leadership" },
 ];
 
 export const attendanceRoutes = routeObject.map(

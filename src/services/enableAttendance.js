b4d2c0ea006a6f4f73dd6c19952b3578bf591349
chidibede <chidibede@gmail.com>
2025-10-21 22:14:07 +0100

    add comment

--- src/services/enableAttendance.js
+++ src/services/enableAttendance.js
@@ -1,7 +1,7 @@
 import apiRequest from "../utils/apiClient";
 
 export const enableAttendance = async () => {
-  // Logic to switch off attendance
+  // Logic to switch on attendance
   const data = await apiRequest("PUT", "/api/attendance/enable");
   return data.data;
 };
--- src/services/history.js
+++ src/services/history.js
@@ -1,6 +1,7 @@
 import apiRequest from "../utils/apiClient";
 
 export const fetchHistoryOptions = async () => {
+  // comment
   const data = await apiRequest("GET", "/api/uniquedates");
   if (!data || data.error) {
     throw new Error(data?.error || "Failed to fetch history options");

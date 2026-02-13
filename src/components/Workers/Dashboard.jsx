4f9a9a63cbd557ac98e8de09ab87d573640a513b
chidibede <chidibede@gmail.com>
2026-02-13 20:13:56 +0100

    redeployin

--- src/components/Workers/Dashboard.jsx
+++ src/components/Workers/Dashboard.jsx
@@ -79,6 +79,7 @@ export default function Dashboard() {
       setAttendanceSummary(calculateTotals(filtered));
       setIsLoading(false);
     });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const debouncedSetActiveGroup = debounce(
